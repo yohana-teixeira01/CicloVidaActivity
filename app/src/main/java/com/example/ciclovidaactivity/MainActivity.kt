80aa73d52fbada0b0a4fba5deda06e4b728d765e
yohana-teixeira01 <yteixeira133@gmail.com>
2023-03-23 16:04:40 -0300

    Subindo Arquivos

---
+++ app/src/main/java/com/example/ciclovidaactivity/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.ciclovidaactivity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ciclovidaactivity.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonPrimeira.setOnClickListener {
+            startActivity(Intent(this, MainActivity2::class.java))
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
---
+++ app/src/main/java/com/example/ciclovidaactivity/MainActivity2.kt
@@ -0,0 +1,38 @@
+package com.example.ciclovidaactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ciclovidaactivity.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMain2Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonVoltar.setOnClickListener {
+            finish()
+        }
+    }
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
